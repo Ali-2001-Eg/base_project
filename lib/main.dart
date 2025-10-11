b513e24c45f141b92e2a91af2e0ffc75ab1736f2
alimazen <alimazen2001ali@gmail.com>
2025-10-11 18:11:05 +0300

    newer version of newFeature.sh

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         designSize: const Size(750, 1334),
